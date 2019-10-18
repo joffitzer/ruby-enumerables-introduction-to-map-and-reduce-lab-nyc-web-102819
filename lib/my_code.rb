c65a71c76c9ddbc488f012f1ec97e95ca80f24f0
Jonah Offitzer <joffitzer@gmail.com>
2019-10-18 01:19:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,7 @@ end
 def reduce_to_all_true(array)
     counter = 0
   while counter < numbers.length do
-      return false if array[counter] = false
+      return false if array[counter] == false
     counter += 1
   end
 end
